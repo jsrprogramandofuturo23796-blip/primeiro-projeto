0aed3cc3028830f48d215b9416fd710f9e217a33
joãodossantos <jsrprogramandofuturo23796@gmail.com>
2026-07-23 13:42:13 -0300

    continuidade do projeto brigadeiro

--- progeto2despertar.html/docomento.js
+++ progeto2despertar.html/docomento.js
@@ -1,9 +1,62 @@
-const memoria = document.getElementsByClassName("descrisao")
+
+function sortear() {
+    const min = Math.ceil(document.querySelector('.input1').value);
+    const max = Math.floor(document.querySelector('.input2').value);
+const result = Math.floor(Math.random() * (max - min + 1)) + min;
+
+let resultadoElemento = document.querySelector('.resultadop');
+resultadoElemento.textContent = `O número sorteado é: ${result}`;
+}
 
 
-console.log(memoria);
 
 
-const issoaqui = document.getElementsByClassName("oimaginario")
+/*function adicionarGasto() {
+const inportaorigem = document.querySelector(`.valordogasto`).value
 
-console.log(issoaqui)
+
+
+console.log(inportaorigem)
+
+
+
+
+
+
+}
+
+
+
+
+
+
+const inptOrigem = document.querySelector('.valordogasto');
+const inptDestino = document.querySelector('.descrisaodogasto');
+if (inptOrigem && inptDestino) {
+    inptDestino.value = inptOrigem.value;}*/
+
+
+function adicionarGasto() {
+const inptOrigem = document.querySelector('.valordogasto');
+const inptDestino = document.querySelector('.descrisaodogasto');
+if (inptOrigem && inptDestino) {
+    inptDestino.value = inptOrigem.value;}
+}
+
+
+
+
+
+
+
+
+/*function calculargasto() {
+const inputValorMensal = document.querySelector(`.descrisaodogasto`);
+const novoGastoDiario = parseFloat
+const valorAtualMensal = parseFloat
+const novoTotal = valorAtualMensal
+inputValorMensal.value = novoTotal
+}
+
+
+
